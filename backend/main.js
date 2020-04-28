4bbb362ca3785314482ee1852c461d9c3eaad09f
Enea Guidi <guidienea@gmail.com>
2020-04-28 11:46:32 +0200

    Added Express.js as backend dependencies

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
