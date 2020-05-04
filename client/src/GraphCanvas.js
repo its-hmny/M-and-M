0564ec364125def05005600e0eb9dc2e971d5331
Enea Guidi <guidienea@gmail.com>
2020-05-04 17:42:09 +0200

    Fixed addEdge and addNode

--- client/src/GraphCanvas.js
+++ client/src/GraphCanvas.js
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import Graph from "react-graph-vis";
 
 const options = {
+  autoresize: true,
   layout: { hierarchical: true },
-  edges: {
-    color: "#000000",
-  },
-
+  edges: { color: "#000000" },
+  manipulation: { enabled: false },
   physic: { enabled: false },
   height: "500px",
 };
@@ -17,31 +16,33 @@ const events = {
   },
 };
 
+let group_id = 1;
+
+
 function GraphCanvas() {
   const [graphNodes, setNodes] = useState([]);
   const [graphEdges, setEdges] = useState([]);
-
   const graph = { nodes: graphNodes, edges: graphEdges };
-  let group_id = 1;
 
   const addNode = () => {
     group_id += 1;
     let last_id = graphNodes.length
-    setNodes(graphNodes.concat({ id: last_id, label: "Added", title: "Title"}));
-    //setNodes([...graphNodes, { id: 1, label: "Added", title: "Title"}]);
+    setNodes([...graphNodes, { id: last_id, label: "Added", title: "Title"}]);
   };
 
   const addEdge = () => {
-    let start_node = 0;
-    let dest_node = 2;
+    let start_node = 1;
+    let dest_node = 0;
+    group_id += 1;
     setEdges([...graphEdges, { from: start_node, to: dest_node }])
   };
 
-  //setNodes([{ id: 1, label: "Added", title: "Title"}]);
+
   return (
     <div id="graph-container">
       <Graph key={group_id} graph={graph} options={options} events={events} />
       <button onClick={addNode}>Add a node</button>
+      <button onClick={addEdge}>Add an edge</button>
     </div>
   );
 }
