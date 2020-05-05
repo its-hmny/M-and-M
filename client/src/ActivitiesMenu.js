4fbe4755d23a4d4f2a7160effb11f7bd7ddc51d7
Enea Guidi <guidienea@gmail.com>
2020-05-05 16:00:59 +0200

    Fixed error in ActivitiesMenu.js

--- client/src/ActivitiesMenu.js
+++ client/src/ActivitiesMenu.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
 
 function ActivitiesMenu() {
   return (
     <div>
       <ExpansionPanel>
+
+        <ExpansionPanelSummary>
+          <Typography>Activities</Typography>
+        </ExpansionPanelSummary>
+
+        <ExpansionPanelDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </ExpansionPanelDetails>
         
       </ExpansionPanel>
     </div>
