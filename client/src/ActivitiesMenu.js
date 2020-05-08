4b06f4b51f77fe984eec5e71fad8f879ec165b72
Enea Guidi <guidienea@gmail.com>
2020-05-08 12:41:34 +0200

    Refractored activity bar and graph component

--- client/src/ActivitiesMenu.js
+++ client/src/ActivitiesMenu.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 
 function ActivitiesMenu() {
   return (
     <div>
       <ExpansionPanel>
-
         <ExpansionPanelSummary>
           <Typography>Activities</Typography>
         </ExpansionPanelSummary>
-
-        <ExpansionPanelDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
-        </ExpansionPanelDetails>
         
+        <div>
+          <h1>Eccoci qua</h1>
+          <p>Questo è un paragrafo</p>
+        </div>
+
+        <div>
+          <h1>Eccoci qua</h1>
+          <p>Questo è un paragrafo</p>
+        </div>
+
       </ExpansionPanel>
     </div>
   );
--- client/src/Editor.js
+++ client/src/Editor.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import GraphCanvas from './GraphCanvas';
 import ActivitiesMenu from './ActivitiesMenu';
+import React, { useState } from "react";
+import Graph from "react-graph-vis";
+import options from "./GraphPreferences.json";
+
+const events = {
+  select: function (event) {
+    //let { nodes, edges } = event;
+  },
+};
+
+let graph_uuid = 1;
 
 function Editor() {
+  const [graphNodes, setNodes] = useState([]);
+  const [graphEdges, setEdges] = useState([]);
+
+  const addNode = () => {
+    graph_uuid += 1;
+    let last_id = graphNodes.length
+    setNodes([...graphNodes, { id: last_id, label: "Added", title: "Title"}]);
+  };
+
+  const addEdge = () => {
+    let start_node = 1;
+    let dest_node = 0;
+    graph_uuid += 1;
+    setEdges([...graphEdges, { from: start_node, to: dest_node }])
+  };
+
+
   return (
     <div>
       <ActivitiesMenu />
-      <GraphCanvas />
+      <Graph key={graph_uuid} graph={{ nodes: graphNodes, edges: graphEdges }} options={options} events={events} />
+      <button onClick={addNode}>Add a node</button>
+      <button onClick={addEdge}>Add an edge</button>
     </div>
   );
 }
