9b6f814646e5c6c279168e70dfe92a4e193a349b
Paolo Marzolo <pollo.marzo@gmail.com>
2020-06-30 18:02:54 +0200

    started working on pin

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import NumberField from './widgets/NumberField';
+import Pin from './widgets/Pin';
 
 function App() {
   return (
     <div className="App">
-      <NumberField />
+      < Pin digits={3} />
     </div>
   );
 }
