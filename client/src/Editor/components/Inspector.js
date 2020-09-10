e88b6e64a0c6bb824069f9f406f7ffacbb791a88
Enea Guidi <guidienea@gmail.com>
2020-09-10 17:40:42 +0200

    Fixed bug, and indented correctly code inside Inspector.js

--- client/src/Editor/components/Inspector.js
+++ client/src/Editor/components/Inspector.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Paper, Box, Typography, List, ListItem, Collapse, Divider } from '@material-ui/core';
 import EditorContext from '../context/EditorContext';
 import properties from '../../ComponentProperties.json';
@@ -8,12 +8,12 @@ import InspectorElement from "./InspectorElement";
 
 const useStyles = makeStyles({
   
-  InspectorPaper:{
+  InspectorPaperStyle:{
     padding: 15,
     zIndex: 2
   },
 
-  InspectorElement:{
+  InspectorElementStyle:{
     margin: 10
   }
   
@@ -21,15 +21,15 @@ const useStyles = makeStyles({
 
 const Inspector = () => {
   const { story, workingActivity } = useContext(EditorContext);
-  const {InspectorPaper, InspectorElement } = useStyles();
-  const [open, setOpen] = React.useState({});
+  const {InspectorPaperStyle, InspectorElementStyle } = useStyles();
+  const [open, setOpen] = useState({});
   const isVisible = workingActivity !== undefined;
   
  
-  const handleClick = (event, index) => {
-    if (open[index] === undefined)
+  const handleClick = (index) => {
+    if (open[index] === undefined) 
       setOpen({...open, [index] : true});
-    else
+    else 
       setOpen({...open, [index] : !open[index]});
   };
   
@@ -43,47 +43,41 @@ const Inspector = () => {
   
   const populateInspector = (iterator, previousPath) => {
     const absPath = previousPath || ["view", "children"];
-    
+
     return (iterator.map((element, index) => {
-        const { mandatory, optional } = properties.components[element.component];
-        const InspectorSection = (
-          <InspectorElement 
-            fieldList={[...mandatory, ...optional]} 
-            pathToComponent={[...absPath, index]}
-          />);
+      const { mandatory, optional } = properties.components[element.component];
+      const InspectorSection = (
+        <InspectorElement  fieldList={[...mandatory, ...optional]} pathToComponent={[...absPath, index]} />
+      );
 
-        return (
-          <Box key={getComponentName(element) + index} className={InspectorElement}>
-            <List className={InspectorElement} >
+      return (
+        <Box key={getComponentName(element) + index} className={InspectorElementStyle}>
+          <List className={InspectorElementStyle} >
               
-              <ListItem button onClick={ (event) => handleClick(event,getComponentName(element)+'-'+iterator+'-'+index)}>
-                <Typography variant="h5" component="h5">{getComponentName(element)}</Typography>
-              </ListItem>
+            <ListItem button onClick={(event) => handleClick(getComponentName(element)+'-'+iterator+'-'+index)}>
+              <Typography variant="h5" component="h5">{getComponentName(element)}</Typography>
+            </ListItem>
 
-              <Collapse in={open[getComponentName(element)+'-'+iterator+'-'+index]} unmountOnExit>  
-                <Box borderLeft={1} borderColor="primary.main">
-                  {InspectorSection}
-                  { element.children instanceof Array ? populateInspector(element.children, [...absPath, index, "children"]) : undefined }
-                </Box>
-              </Collapse>
+            <Collapse in={open[getComponentName(element)+'-'+iterator+'-'+index]} unmountOnExit>  
+              <Box borderLeft={1} borderColor="primary.main">
+                {InspectorSection}
+                { Array.isArray(element.children) ? populateInspector(element.children, [...absPath, index, "children"]) : undefined }
+              </Box>
+            </Collapse>
 
-            </List>
-
-            <Divider/>
-            
-          </Box>
-        );
+          </List>
+          <Divider/>
+        </Box>
+     );
     }));
   };
   
   return (
-      <Paper className={InspectorPaper} elevation={3}>
-        { (!isVisible) ? 
-          <Typography variant="h5" component="h5">
-            Select an element
-          </Typography> :
-          
-          populateInspector(story.nodes.filter(node => node.id === workingActivity)[0].view.children)
+      <Paper className={InspectorPaperStyle} elevation={3}>
+        { 
+          (!isVisible) ? 
+            <Typography variant="h5" component="h5">Select an element</Typography> :
+            populateInspector(story.nodes.filter(node => node.id === workingActivity)[0].view.children)
         }
       </Paper>
     
