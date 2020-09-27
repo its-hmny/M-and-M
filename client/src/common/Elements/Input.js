e728d6204ec1475a19047c21b99d54826d62b1f7
Enea Guidi <guidienea@gmail.com>
2020-09-27 18:55:46 +0200

    Woops

---
+++ client/src/common/Elements/Input.js
@@ -0,0 +1,29 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { useState } from 'react';
+
+const base = css`
+  border-radius: 5px;
+`;
+
+const Input = ({ inputType, placeholder, correctAnswer, text, style }) => {
+  const [answer, setAnswer] = useState("");
+
+  const validate = () => {
+    if (answer.toLowerCase() === correctAnswer.toLowerCase())
+      alert("Answer correct")
+    else 
+      alert("Answer wrong")
+  }
+  
+  return (
+    <div>
+      <input type={inputType} placeholder={placeholder} value={answer} 
+        onChange={event => setAnswer(event.target.value)} css={[css, base]}
+      />
+      <button onClick={validate}>Invia risposta</button>
+    </div>
+  );
+};
+
+export default Input;
