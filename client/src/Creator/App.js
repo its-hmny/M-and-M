dc9cd0bfc0b03eab16c3eebba6a69513a900867f
Paolo Marzolo <pollo.marzo@gmail.com>
2021-01-17 02:37:27 +0100

    included notification on saved in creator

---
+++ client/src/Creator/App.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Grid, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Preview from '../common/Preview';
+import Inspector from './components/Inspector';
+import SaveDialog from './components/SaveDialog';
+import axios from '../common/shared';
+import useTemplateStore from './stores/template';
+import useStylesStore from './stores/styles';
+import { useSnackbar } from 'notistack';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  grid: {
+    flexGrow: 1,
+  },
+  previewPanel: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+}));
+
+const App = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const classes = useStyles();
+  const [isSaving, setIsSaving] = useState(false);
+  const styles = useStylesStore(state => state.styles);
+  const components = useTemplateStore(state => state.components);
+  const saveTemplate = meta => {
+    setIsSaving(false);
+    const styledComponents = components.map(component => ({
+      ...component,
+      style: styles[component.styleId],
+    }));
+    axios // See common/shared.js (useAPI hook)
+      .post('templates', { ...meta, components: styledComponents })
+      .then(value => {
+        console.log('saved template ', value);
+        enqueueSnackbar('saved your template!', { variant: 'success' });
+      })
+      .catch(err => {
+        console.error(err);
+        enqueueSnackbar('something went wrong...', { variant: 'error' });
+      });
+  };
+
+  return (
+    <div className={classes.container}>
+      <Grid container className={classes.grid}>
+        <Grid item xs={6}>
+          <Inspector />
+        </Grid>
+        <Grid xs={6} item>
+          <div className={classes.previewPanel}>
+            <Preview components={components} styles={styles} />
+            <Button
+              color="primary"
+              variant="contained"
+              className={classes.saveButton}
+              onClick={() => setIsSaving(true)}
+            >
+              Save Template
+            </Button>
+            <SaveDialog
+              open={isSaving}
+              onCancel={() => {
+                setIsSaving(false);
+              }}
+              onSave={saveTemplate}
+            />
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default App;
--- client/src/Creator/index.js
+++ client/src/Creator/index.js
@@ -1,79 +1,19 @@
-import React, { useState } from 'react';
-import { Grid, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { SnackbarProvider } from 'notistack';
+import App from './App';
+import React from 'react';
 
-import Preview from '../common/Preview';
-import Inspector from './components/Inspector';
-import SaveDialog from './components/SaveDialog';
-import axios from '../common/shared';
-import useTemplateStore from './stores/template';
-import useStylesStore from './stores/styles';
-
-const useStyles = makeStyles(theme => ({
-  container: {
-    width: '100vw',
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  grid: {
-    flexGrow: 1,
-  },
-  previewPanel: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-}));
-
-const App = () => {
-  const classes = useStyles();
-  const [isSaving, setIsSaving] = useState(false);
-  const styles = useStylesStore(state => state.styles);
-  const components = useTemplateStore(state => state.components);
-  const saveTemplate = meta => {
-    setIsSaving(false);
-    const styledComponents = components.map(component => ({
-      ...component,
-      style: styles[component.styleId],
-    }));
-    axios // See common/shared.js (useAPI hook)
-      .post('templates', { ...meta, components: styledComponents })
-      .then(value => console.log(value))
-      .catch(err => console.error(err));
-  };
-
+const Creator = () => {
   return (
-    <div className={classes.container}>
-      <Grid container className={classes.grid}>
-        <Grid item xs={6}>
-          <Inspector />
-        </Grid>
-        <Grid xs={6} item>
-          <div className={classes.previewPanel}>
-            <Preview components={components} styles={styles} />
-            <Button
-              color="primary"
-              variant="contained"
-              className={classes.saveButton}
-              onClick={() => setIsSaving(true)}
-            >
-              Save Template
-            </Button>
-            <SaveDialog
-              open={isSaving}
-              onCancel={() => {
-                setIsSaving(false);
-              }}
-              onSave={saveTemplate}
-            />
-          </div>
-        </Grid>
-      </Grid>
-    </div>
+    <SnackbarProvider
+      maxSnack={10}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+    >
+      <App />
+    </SnackbarProvider>
   );
 };
 
-export default App;
+export default Creator;
