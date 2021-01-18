26928485122c51c1bce51183d4d01186af10eb59
Enea Guidi <guidienea@gmail.com>
2021-01-18 22:30:50 +0100

    Added navigation between different ambient

--- client/src/Evaluator/App.js
+++ client/src/Evaluator/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Fab, makeStyles } from '@material-ui/core';
+import { Home as HomeIcon } from '@material-ui/icons';
 
 import { ActivePlayersList, ProgressGraph, WidgetArea } from './components/';
-import { makeStyles } from '@material-ui/core';
+
+import * as ROUTES from '../routes';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -16,14 +20,33 @@ const useStyles = makeStyles(theme => ({
       outline: 'none',
     },
   },
+  navButton: {
+    position: 'absolute',
+    zIndex: 1,
+    margin: theme.spacing(2),
+    marginLeft: theme.spacing(30),
+  },
+  navIcon: {
+    marginRight: theme.spacing(1),
+  },
 }));
 
 const App = () => {
+  const history = useHistory();
   const classes = useStyles();
+
   return (
     <div className={classes.container}>
       <div className={classes.main}>
         <ActivePlayersList />
+        <Fab
+          className={classes.navButton}
+          onClick={() => history.push(ROUTES.HOME)}
+          variant="extended"
+        >
+          <HomeIcon className={classes.navIcon} />
+          Home
+        </Fab>
         <ProgressGraph />
       </div>
       <WidgetArea />
